e3116c9f2d6915dfaeccf62dba01b689af5b1626
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-03-30 08:40:20 +0200

    Add some sort of license.

--- ql.lua
+++ ql.lua
@@ -1,3 +1,20 @@
+--[[
+License:
+Copyright 2010 Stein-Ivar Berghei (irc:steino@freenode)
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+
 function parse(file)
 	local out = {}
 	file = assert(io.open(file, "r"))
@@ -24,7 +41,7 @@ function np(data, buffer, args)
 	end
 end
 
-weechat.register("np_quodlibet", "steino", "0.1", "GPL-3", "Nowplaying script for Quod Libet", "", "")
+weechat.register("np_quodlibet", "steino", "0.1", "Apache-2.0", "Nowplaying script for Quod Libet", "", "")
 weechat.hook_command("np",
 "Prints the current song to current buffer.",
 "",
