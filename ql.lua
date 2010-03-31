d7f68daff8a724423b4b50923c3412b4e312c3f6
Stein-Ivar Berghei <stein-ivar@berghei.no>
2010-03-31 11:19:34 +0200

    Print error message if QL isnt running or is stopped

--- ql.lua
+++ ql.lua
@@ -25,27 +25,34 @@ QL = {
 
 function parse(file)
 	local out = {}
-	file = assert(io.open(file, "r"))
-	for line in file:lines() do
-		for i,v in line:gmatch("(%a+)=(.+)") do
-			out[i] = v
+	file = io.open(file, "r")
+	if file then
+		for line in file:lines() do
+			for i,v in line:gmatch("(%a+)=(.+)") do
+				out[i] = v
+			end
 		end
+		local min = string.format("%1.f", math.floor(out["length"]/60))
+		local sec = string.format("%02.f", math.floor(out["length"]-(min*60))) 
+		out["ttime"] =  min .. ":" .. sec
+		file:close()
+		return out
+	else
+		return nil
 	end
-	local min = string.format("%1.f", math.floor(out["length"]/60))
-	local sec = string.format("%02.f", math.floor(out["length"]-(min*60))) 
-	out["ttime"] =  min .. ":" .. sec
-	file:close()
-
-	return out
 end
 
 function np(data, buffer, args)
 	local int = weechat.buffer_get_integer(buffer, "number")
 	local data = parse(os.getenv("HOME") .. "/.quodlibet/current")
-	if int == 1 then
-		weechat.print(buffer, string.format("np: %s - [%s] %s (%s/%s)", data["artist"], data["album"], data["title"], data["ttime"], data["format"]))
+	if data then
+		if int == 1 then
+			weechat.print(buffer, string.format("np: %s - [%s] %s (%s/%s)", data["artist"], data["album"], data["title"], data["ttime"], data["format"]))
+		else
+			weechat.command(buffer, string.format("np: %s - [%s] %s (%s/%s)", data["artist"], data["album"], data["title"], data["ttime"], data["format"]))
+		end
 	else
-		weechat.command(buffer, string.format("np: %s - [%s] %s (%s/%s)", data["artist"], data["album"], data["title"], data["ttime"], data["format"]))
+		weechat.print(buffer, "Could not open file, is quodlibet running?")
 	end
 end
 
